0781547c096a7c27caa63870f02523636cbb7ca5
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-06-15 12:29:14 -0400

    add tests for games with DCs and bad data

--- stats/gsheets.py
+++ stats/gsheets.py
@@ -1,31 +1,41 @@
 import json
+from typing import Any, List
 import pandas as pd
 from pathlib import Path
 
 
-def json_as_df(path_to_json: Path, str_cols=[]) -> pd.DataFrame:
+def json_as_df(path_to_json: Path, str_cols: List[str] = []) -> pd.DataFrame:
     """Return a DataFrame with cleaned up column names. Cases are lowered, spaces are turned into `_`, and periods are removed. E.g. `A. AB` turns into `a_ab`
 
     Args:
         path_to_json Path
         str_cols these columns should be treated as strings and not coerced to numeric
     """
-    # read data
     with open(path_to_json, "r") as f:
         raw_data = json.loads(f.read())
 
-    # make the dataframe
-    df = pd.DataFrame(raw_data["values"][1:])
+    return values_to_df(raw_data["values"], str_cols)
+
+
+def values_to_df(values: List[List[Any]], str_cols: List[str] = []) -> pd.DataFrame:
+    """Make the actual DataFrame. For column names, cases are lowered, spaces are turned into `_`, and periods are removed. E.g. `A. AB` turns into `a_ab`
+
+    Args:
+        values list of lists where the first element is the column labels
+        str_cols these columns should be treated as strings and not coerced to numeric
+    Returns:
+        pd.DataFrame
+    """
+    df = pd.DataFrame(values[1:])
     columns = [
-        col.lower().strip().replace(" ", "_").replace(".", "")
-        for col in raw_data["values"][0]
+        str(col).lower().strip().replace(" ", "_").replace(".", "") for col in values[0]
     ]
     df.columns = columns
 
     # fix types
     numeric_cols = [col for col in columns if col not in str_cols]
     for col in numeric_cols:
-        df[col] = pd.to_numeric(df[col], errors="raise")
+        df[col] = pd.to_numeric(df[col], errors="coerce")
     if str_cols:
         df[str_cols] = df[str_cols].astype("string")
 
--- tests/stats/test_games.py
+++ tests/stats/test_games.py
@@ -1,37 +1,41 @@
 import pandas as pd
+import numpy as np
 import unittest
 
+from stats import gsheets
 from stats.games import agg_team_stats, annotate_game_results, annotate_computed_stats
+
+box_score_columns = [
+    "week",
+    "away",
+    "a_score",
+    "h_score",
+    "home",
+    "a_e",
+    "h_e",
+    "ip",
+    "a_ab",
+    "a_r",
+    "a_h",
+    "a_hr",
+    "a_rbi",
+    "a_bb",
+    "a_so",
+    "h_ab",
+    "h_r",
+    "h_h",
+    "h_hr",
+    "h_rbi",
+    "h_bb",
+    "h_so",
+]
 
 
 class TestGames(unittest.TestCase):
     def test_mix_of_good_and_dc(self):
-        columns = [
-            "week",
-            "away",
-            "a_score",
-            "h_score",
-            "home",
-            "a_e",
-            "h_e",
-            "ip",
-            "a_ab",
-            "a_r",
-            "a_h",
-            "a_hr",
-            "a_rbi",
-            "a_bb",
-            "a_so",
-            "h_ab",
-            "h_r",
-            "h_h",
-            "h_hr",
-            "h_rbi",
-            "h_bb",
-            "h_so",
-        ]
         box_scores_df = pd.DataFrame(
             [
+                # typical DC box score
                 [
                     1,
                     "alligators",
@@ -42,40 +46,113 @@ class TestGames(unittest.TestCase):
                     0,
                     9,
                 ],
+                # good
                 [
-                    1,
-                    "hippos",
-                    1,
-                    2,
-                    "alligators",
-                    0,
-                    0,
-                    9,
-                    30,
-                    1,
-                    1,
-                    1,
-                    0,
-                    1,
-                    10,
-                    30,
-                    2,
-                    2,
-                    2,
-                    0,
-                    4,
-                    12,
+                    1,  # week
+                    "hippos",  # away
+                    1,  # a_score
+                    2,  # h_score
+                    "alligators",  # home
+                    0,  # a_e
+                    0,  # h_e
+                    9,  # ip
+                    30,  # a_ab
+                    1,  # a_r
+                    1,  # a_h
+                    1,  # a_hr
+                    0,  # a_rbi
+                    1,  # a_bb
+                    10,  # a_so
+                    30,  # h_ab
+                    2,  # h_r
+                    2,  # h_h
+                    2,  # h_hr
+                    0,  # h_rbi
+                    4,  # h_bb
+                    12,  # h_so
                 ],
             ],
-            columns=columns,
+            columns=box_score_columns,
         )
 
         annotate_game_results(box_scores_df, False)
         team_stats_df = agg_team_stats(box_scores_df)
         annotate_computed_stats(team_stats_df, league="TEST", league_era=1.0)
 
+        ba: float = team_stats_df.at["hippos", "ba"]
+
         self.assertAlmostEqual(
-            team_stats_df.loc["hippos"].ba,
+            ba,
             1 / 30,
-            msg="Hippos batted 1/30 in the game that wasn't a DC",
+            msg="hippos batted 1/30 in the game that wasn't a DC",
+        )
+
+    def test_garbage_in(self):
+        box_scores_df = gsheets.values_to_df(
+            [
+                box_score_columns,
+                # garbage
+                [
+                    1,  # week
+                    "alligators",  # away
+                    1,  # a_score
+                    2,  # h_score
+                    "hippos",  # home
+                    0,  # a_e
+                    0,  # h_e
+                    9,  # ip
+                    "bad entry",  # a_ab
+                    1,  # a_r
+                    1,  # a_h
+                    1,  # a_hr
+                    0,  # a_rbi
+                    1,  # a_bb
+                    10,  # a_so
+                    30,  # h_ab
+                    2,  # h_r
+                    2,  # h_h
+                    2,  # h_hr
+                    0,  # h_rbi
+                    4,  # h_bb
+                    12,  # h_so
+                ],
+                # good
+                [
+                    1,  # week
+                    "hippos",  # away
+                    1,  # a_score
+                    2,  # h_score
+                    "alligators",  # home
+                    0,  # a_e
+                    0,  # h_e
+                    9,  # ip
+                    30,  # a_ab
+                    1,  # a_r
+                    1,  # a_h
+                    1,  # a_hr
+                    0,  # a_rbi
+                    1,  # a_bb
+                    10,  # a_so
+                    30,  # h_ab
+                    2,  # h_r
+                    2,  # h_h
+                    2,  # h_hr
+                    0,  # h_rbi
+                    4,  # h_bb
+                    12,  # h_so
+                ],
+            ],
+            str_cols=["away", "home"],
+        )
+
+        annotate_game_results(box_scores_df, False)
+        team_stats_df = agg_team_stats(box_scores_df)
+        annotate_computed_stats(team_stats_df, league="TEST", league_era=1.0)
+
+        ba: float = team_stats_df.at["alligators", "ba"]
+
+        self.assertAlmostEqual(
+            ba,
+            3 / 30,
+            msg="alligators had 3 hits and 30 known ABs (one game is missing ABs)",
         )
